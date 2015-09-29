a4661c1e58e0b87d9c241785cf8ed0f44aeeef95
nehuang1 <nehuang1@galileoweb.org>
2015-09-29 02:31:38 +0000

    This is my commmit message

--- GeometryLab.java
+++ GeometryLab.java
@@ -2,65 +2,65 @@ public class GeometryLab {
     public int f(int x, int y) {
         return x + y;
     }
-    public int g (int x, int y) {
+    public int g(int x, int y) {
         return x - y;
-    }    
-    public int h (int x, int y) {
+    }
+    public int h(int x, int y) {
         return x * y;
     }
-    public int j (int x, int y) {
+    public int j(int x, int y) {
         return x / y;
     }
-    public int k (int x, int y) {
-        return x / y;
+    public int k(int x, int y) {
+        return x % y;
     }
-    public int m (int x, int y, int z) {
+    public int m(int x, int y, int z) {
         return x + y - z;
     }
-    public int triArea (int b, int h) {
+    public int triArea(int b , int h) {
         return 1/2 * b * h;
     }
-    public int trapArea (int b1, int b2, int h) {
-        return 1/2 * h * (b1 + b2);
+    public int trapArea(int b1, int b2, int h) {
+        return 1/2 * h * (b1 +b2);
     }
-    public int rectArea (int b, int h) {
+    public int rectArea(int b, int h) {
         return b * h;
     }
-    public int circArea (int r) {
-        return 3.14 * r * r;
+    public double circArea(int r) {
+        return 3.14 * r * r ;
     }
-    public int paralArea (int b, int h) {
+    public int paralArea(int b, int h) {
         return b * h;
     }
-    public int pythHypotenuse (int a, int b) {
-        return a * a + b * b;
+    public int pythHypotenuse ( int a, int b) {
+        return a*a + b*b;
     }
     public int triPrismVol (int b, int h, int w) {
-        return 1/2 * b * h * w;
+        return 1/2 * b * h * w; 
     }
     public int rectPrismVol (int b, int h, int w) {
         return b * h * w;
     }
-    public int rectPyramidVol (int b, int h, int w) {
+    public int rectPyramidVol(int b , int h, int w) {
         return 1/3 * b * h * w;
     }
-    public int cylVol (int r, int h) {
+    public double cylVol(int r, int h) {
         return 3.14 * r * r * h;
     }
-    public int sphereVol (int r) {
+    public double sphereVol(int r) {
         return 4/3 * 3.14 * r * r * r;
     }
-    public int coneVol (int r, int h) {
+    public double coneVol(int r, int h) {
         return 1/3 * 3.14 * r * r * h;
     }
-    public int sphereSurfArea (int r) {
-        return 4 * 3.14 * r * r;
-    }    
-    public int cylSurfArea (int r, int h) {
+    public double sphereSurfArea( int r) {
+        return 4 * 3.14 *r * r ;
+    }
+    public double cylSurfArea ( int r, int h) {
         return 2 * 3.14 * r * h + 2 * 3.14 * r * r;
     }
-    public int PrismSurfArea (int b, int h, int w) {
-        return 2 * b * h + 2 * b * w + 2 * h * w;
+    public int rectPrismSurfArea ( int b, int h, int w) {
+        return 2 *b *h + 2 *b *w +2 *h * w;
     }
 }
 
